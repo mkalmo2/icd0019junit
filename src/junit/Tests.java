8d83cded89b4793659ce1f11ee5a4e8aea55b8f1
Märt Kalmo <mart.kalmo@gmail.com>
2019-02-09 15:05:32 +0200

    fixed imports

--- src/junit/Tests.java
+++ src/junit/Tests.java
@@ -2,8 +2,8 @@ package junit;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 public class Tests {
